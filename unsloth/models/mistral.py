d6b3538684e62b7e261fb6b52cd0ebfd1f353ca5
Adalberto <multiolx@gmail.com>
2024-03-24 11:48:54 -0300

    Update mistral.py

--- unsloth/models/mistral.py
+++ unsloth/models/mistral.py
@@ -248,7 +248,7 @@ class FastMistralModel(FastLlamaModel):
         MistralSdpaAttention  .forward = MistralAttention_fast_forward
         MistralFlashAttention2.forward = MistralAttention_fast_forward
         MistralDecoderLayer   .forward = LlamaDecoderLayer_fast_forward
-        MistralMLP            .forward = LlamaMLP_fast_forward
+        # MistralMLP            .forward = LlamaMLP_fast_forward
         MistralModel          .forward = LlamaModel_fast_forward
         MistralForCausalLM    .forward = MistralForCausalLM_fast_forward
         PeftModelForCausalLM  .forward = PeftModelForCausalLM_fast_forward
